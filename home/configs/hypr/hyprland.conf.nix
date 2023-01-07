7c8f0872b5a496c3aed76603e6bf409991e25da6
Élysæ <101974839+ElysaSrc@users.noreply.github.com>
2023-01-07 11:08:50 +0100

    Fixed hyprland borders

--- home/configs/hypr/hyprland.conf.nix
+++ home/configs/hypr/hyprland.conf.nix
@@ -20,8 +20,8 @@ general {
     gaps_in = 5
     gaps_out = 20
     border_size = 2
-    col.active_border = rgba(${c.green}ee) rgba(${c.white}ee) 45deg
-    col.inactive_border = rgba(${c.dim.white}aa)
+    col.active_border = rgba(${c.nohex c.green}ee) rgba(${c.nohex c.white}ee) 45deg
+    col.inactive_border = rgba(${c.nohex c.dim.white}aa)
     layout = dwindle
 }
 
